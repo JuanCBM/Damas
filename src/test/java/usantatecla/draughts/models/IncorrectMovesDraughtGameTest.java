5311a83e25d27976bd46207d68b4bbde3ac32702
cd <jc-blazquez@hotmail.com>
2020-11-07 22:18:42 +0100

    Format Incorrect Test

--- src/test/java/usantatecla/draughts/models/IncorrectMovesDraughtGameTest.java
+++ src/test/java/usantatecla/draughts/models/IncorrectMovesDraughtGameTest.java
@@ -1,18 +1,8 @@
 package usantatecla.draughts.models;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Before;
 import org.junit.Test;
-
-// Test de todos los posibles errores al mover
-/*
- * Error!!! No te entiendo: <d><d>{,<d><d>}[0-2] Error!!! No es una coordenada del tablero
- * Error!!! No hay ficha que mover Error!!! No es una de tus fichas Error!!! No vas en diagonal
- * Error!!! No está vacío el destino Error!!! No comes contrarias Error!!! No se puede comer
- * tantas en un movimiento Error!!! No avanzas Error!!! No respetas la distancia Error!!! No se
- * puede comer tantas en un salto
- */
 
 //@formatter:off
 public class IncorrectMovesDraughtGameTest {
@@ -30,150 +20,171 @@ public class IncorrectMovesDraughtGameTest {
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEThenOPPOSITE_PIECE() {
+  public void testGivenGameWhenMoveThenOPPOSITE_PIECE() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        " N      ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.OPPOSITE_PIECE,game,
-        new Coordinate(2, 1),
-        new Coordinate(3, 0));
+                                      "        ",
+                                      "        ",
+                                      " N      ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.OPPOSITE_PIECE, 
+                    game, 
+                    new Coordinate(2, 1), new Coordinate(3, 0));
   }
 
 
   @Test
   public void testGivenGameWhenMoveDownThenNOT_DIAGONAL() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "  B     ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.NOT_DIAGONAL,game,
-        new Coordinate(5, 2),
-        new Coordinate(4, 2));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "  B     ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.NOT_DIAGONAL, 
+                    game,
+                    new Coordinate(5, 2), new Coordinate(4, 2));
   }
 
   @Test
   public void testGivenGameWhenMoveUpThenNOT_DIAGONAL() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "  B     ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.NOT_DIAGONAL,game,
-        new Coordinate(5, 2),
-        new Coordinate(6, 2));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "  B     ",
+                                      "        ",
+                                      "        ")
+                                  .build();
+   
+    assertErrorMove(Error.NOT_DIAGONAL, 
+                    game, 
+                    new Coordinate(5, 2), new Coordinate(6, 2));
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEThenNOT_EMPTY_TARGET() {
+  public void testGivenGameWhenMoveThenNOT_EMPTY_TARGET() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "      N ",
-        "       b",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.NOT_EMPTY_TARGET,game,
-        new Coordinate(4, 7),
-        new Coordinate(3, 6));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "      N ",
+                                      "       b",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.NOT_EMPTY_TARGET, 
+                    game, 
+                    new Coordinate(4, 7), new Coordinate(3, 6));
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEEatingThenNOT_EMPTY_TARGET() {
+  public void testGivenGameWhenMoveEatingThenNOT_EMPTY_TARGET() {    
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "   n    ",
-        "  n     ",
-        " B      ",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.NOT_EMPTY_TARGET,game,
-        new Coordinate(4, 1),
-        new Coordinate(2, 3));
+                                      "        ",
+                                      "        ",
+                                      "   n    ",
+                                      "  n     ",
+                                      " B      ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+
+    assertErrorMove(Error.NOT_EMPTY_TARGET,
+                    game, 
+                    new Coordinate(4, 1), new Coordinate(2, 3));
+    
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEEatingThenCOLLEAGUE_EATING(){
+  public void testGivenGameWhenMoveEatingThenCOLLEAGUE_EATING() {    
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "     b  ",
-        "      B ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.COLLEAGUE_EATING,game,
-        new Coordinate(5, 6),
-        new Coordinate(0, 1));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "     b  ",
+                                      "      B ",
+                                      "        ",
+                                      "        ")
+                                .build();
+
+    assertErrorMove(Error.COLLEAGUE_EATING, 
+                    game, 
+                    new Coordinate(5, 6), new Coordinate(0, 1));
+    
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEEatingThenTOO_MUCH_EATINGS(){
+  public void testGivenGameWhenMoveEatingThenTOO_MUCH_EATINGS() {    
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "   n    ",
-        "        ",
-        "     n  ",
-        "      B ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.TOO_MUCH_EATINGS,game,
-        new Coordinate(5, 6),
-        new Coordinate(0, 1));
+                                      "        ",
+                                      "        ",
+                                      "   n    ",
+                                      "        ",
+                                      "     n  ",
+                                      "      B ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.TOO_MUCH_EATINGS, 
+                    game, 
+                    new Coordinate(5, 6), new Coordinate(0, 1));
+    
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEThenTOO_MUCH_JUMPS() {
+  public void testGivenGameWhenMoveThenTOO_MUCH_JUMPS() {    
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        " B      ",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.TOO_MUCH_JUMPS,game,
-        new Coordinate(4, 1),
-        new Coordinate(3, 2),
-        new Coordinate(2, 3));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      " B      ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.TOO_MUCH_JUMPS, 
+                    game, 
+                    new Coordinate(4, 1), new Coordinate(3, 2), new Coordinate(2, 3));
+    
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEEatingThenTOO_MUCH_JUMPS() {
+  public void testGivenGameWhenMoveEatingThenTOO_MUCH_JUMPS() {    
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "  n     ",
-        " B      ",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.TOO_MUCH_JUMPS,game,
-        new Coordinate(4, 1),
-        new Coordinate(2, 3),
-        new Coordinate(1, 2));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "  n     ",
+                                      " B      ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.TOO_MUCH_JUMPS, 
+                    game,
+                    new Coordinate(4, 1), new Coordinate(2, 3), new Coordinate(1, 2));
+    
   }
 
 }
--- src/test/java/usantatecla/draughts/models/IncorrectMovesPawnGameTest.java
+++ src/test/java/usantatecla/draughts/models/IncorrectMovesPawnGameTest.java
@@ -3,16 +3,11 @@ package usantatecla.draughts.models;
 import static org.junit.Assert.assertEquals;
 import org.junit.Before;
 import org.junit.Test;
+
+
 //@formatter:off
 public class IncorrectMovesPawnGameTest {
-  // Test de todos los posibles errores al mover
-  /*
-   * Error!!! No te entiendo: <d><d>{,<d><d>}[0-2] Error!!! No es una coordenada del tablero
-   * Error!!! No hay ficha que mover Error!!! No es una de tus fichas Error!!! No vas en diagonal
-   * Error!!! No está vacío el destino Error!!! No comes contrarias Error!!! No se puede comer
-   * tantas en un movimiento Error!!! No avanzas Error!!! No respetas la distancia Error!!! No se
-   * puede comer tantas en un salto
-   */
+
   GameBuilder gameBuilder;
 
   @Before
@@ -26,187 +21,201 @@ public class IncorrectMovesPawnGameTest {
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEThenEMPTY_ORIGIN() {
+  public void testGivenGameWhenMoveThenEMPTY_ORIGIN() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.EMPTY_ORIGIN,game,
-        new Coordinate(4, 3),
-        new Coordinate(3, 4));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.EMPTY_ORIGIN,
+                    game, 
+                    new Coordinate(4, 3), new Coordinate(3, 4));
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEThenOPPOSITE_PIECE() {
+  public void testGivenGameWhenMoveThenOPPOSITE_PIECE() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        " n      ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.OPPOSITE_PIECE,game,
-        new Coordinate(2, 1),
-        new Coordinate(3, 0));
+                                      "        ",
+                                      "        ",
+                                      " n      ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.OPPOSITE_PIECE,
+                    game, 
+                    new Coordinate(2, 1), new Coordinate(3, 0));
   }
 
   @Test
   public void testGivenGameWhenMoveDownThenNOT_DIAGONAL() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "  b     ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.NOT_DIAGONAL,game,
-        new Coordinate(5, 2),
-        new Coordinate(4, 2));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "  b     ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.NOT_DIAGONAL,
+                    game,
+                    new Coordinate(5, 2), new Coordinate(4, 2));
   }
 
   @Test
   public void testGivenGameWhenMoveUpThenNOT_DIAGONAL() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "  b     ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.NOT_DIAGONAL,game,
-        new Coordinate(5, 2),
-        new Coordinate(6, 2));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "  b     ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.NOT_DIAGONAL, 
+                    game,
+                    new Coordinate(5, 2), new Coordinate(6, 2));
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEThenNOT_EMPTY_TARGET() {
+  public void testGivenGameWhenMoveThenNOT_EMPTY_TARGET() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "      n ",
-        "       b",
-        "        ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.NOT_EMPTY_TARGET,game,
-        new Coordinate(4, 7),
-        new Coordinate(3, 6));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "      n ",
+                                      "       b",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.NOT_EMPTY_TARGET,
+                    game, 
+                    new Coordinate(4, 7), new Coordinate(3, 6));
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEEatingThenNOT_EMPTY_TARGET() {
+  public void testGivenGameWhenMoveEatingThenNOT_EMPTY_TARGET() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "   n    ",
-        "  n     ",
-        " b      ",
-        "        ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.NOT_EMPTY_TARGET,game,
-        new Coordinate(4, 1),
-        new Coordinate(2, 3));
-  }
-
-
-  @Test
-  public void testGivenGameWhenMoveWHITEThenNOT_ADVANCED() {
-    Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "       b",
-        "        ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.NOT_ADVANCED,game,
-        new Coordinate(4, 7),
-        new Coordinate(5, 6));
-  }
-
-
-  @Test
-  public void testGivenGameWhenMoveWHITEThenWITHOUT_EATING() {
-    Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "    b   ",
-        "        ",
-        "        ").color(Color.WHITE).build();
-    assertErrorMove(Error.WITHOUT_EATING,game,
-        new Coordinate(5, 4),
-        new Coordinate(3, 2));
-  }
-
-
-  @Test
-  public void testGivenGameWhenMoveWHITEThenTOO_MUCH_ADVANCED() {
-    Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        "b       ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.TOO_MUCH_ADVANCED,game,
-        new Coordinate(5, 0),
-        new Coordinate(2, 3));
-  }
-
-
-  @Test
-  public void testGivenGameWhenMoveWHITEThenTOO_MUCH_JUMPS() {
-    Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "        ",
-        " b      ",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.TOO_MUCH_JUMPS,game,
-        new Coordinate(4, 1),
-        new Coordinate(3, 2),
-        new Coordinate(2, 3));
+                                      "        ",
+                                      "        ",
+                                      "   n    ",
+                                      "  n     ",
+                                      " b      ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.NOT_EMPTY_TARGET, 
+                    game,
+                    new Coordinate(4, 1), new Coordinate(2, 3));
   }
 
   @Test
-  public void testGivenGameWhenMoveWHITEEatingThenTOO_MUCH_JUMPS() {
+  public void testGivenGameWhenMoveThenNOT_ADVANCED() {
     Game game = this.gameBuilder.rows(
-        "        ",
-        "        ",
-        "        ",
-        "  n     ",
-        " b      ",
-        "        ",
-        "        ",
-        "        ").build();
-    assertErrorMove(Error.TOO_MUCH_JUMPS,game,
-        new Coordinate(4, 1),
-        new Coordinate(2, 3),
-        new Coordinate(1, 2));
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "       b",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+      assertErrorMove(Error.NOT_ADVANCED,
+                      game,
+                      new Coordinate(4, 7), new Coordinate(5, 6));
   }
-  
-  
+
+  @Test
+  public void testGivenGameWhenMoveThenWITHOUT_EATING() {
+    Game game = this.gameBuilder.rows(
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "    b   ",
+                                      "        ",
+                                      "        ")
+                                  .build();
+    
+    assertErrorMove(Error.WITHOUT_EATING, 
+                    game,
+                    new Coordinate(5, 4), new Coordinate(3, 2));
+  }
+
+  @Test
+  public void testGivenGameWhenMoveThenTOO_MUCH_ADVANCED() {
+    Game game = this.gameBuilder.rows(
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "b       ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.TOO_MUCH_ADVANCED,
+                    game,
+                    new Coordinate(5, 0), new Coordinate(2, 3));
+  }
+
+  @Test
+  public void testGivenGameWhenMoveThenTOO_MUCH_JUMPS() {
+    Game game = this.gameBuilder.rows(
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      " b      ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.TOO_MUCH_JUMPS,
+                    game,
+                    new Coordinate(4, 1), new Coordinate(3, 2), new Coordinate(2, 3));
+  }
+
+  @Test
+  public void testGivenGameWhenMoveEatingThenTOO_MUCH_JUMPS() {
+    Game game = this.gameBuilder.rows(
+                                      "        ",
+                                      "        ",
+                                      "        ",
+                                      "  n     ",
+                                      " b      ",
+                                      "        ",
+                                      "        ",
+                                      "        ")
+                                .build();
+    
+    assertErrorMove(Error.TOO_MUCH_JUMPS,
+                    game,
+                    new Coordinate(4, 1), new Coordinate(2, 3), new Coordinate(1, 2));
+  }
 
 }
