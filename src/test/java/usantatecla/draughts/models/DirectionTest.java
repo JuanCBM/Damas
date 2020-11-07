0decfa3c75bf384f78450df5d50e7c8a06820489
cd <jc-blazquez@hotmail.com>
2020-11-07 17:47:14 +0100

    Estructura de proximos test

---
+++ src/test/java/usantatecla/draughts/models/DirectionTest.java
@@ -0,0 +1,18 @@
+package usantatecla.draughts.models;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DirectionTest {
+
+  @Test
+  public void testIsOnDirection() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetDistanceCoordinate() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ src/test/java/usantatecla/draughts/models/PieceTest.java
@@ -0,0 +1,43 @@
+package usantatecla.draughts.models;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class PieceTest {
+
+  @Test
+  public void testPiece() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsCorrectMovement() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsCorrectDiagonalMovement() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsLimit() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testIsAdvanced() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetColor() {
+    fail("Not yet implemented");
+  }
+
+  @Test
+  public void testGetCode() {
+    fail("Not yet implemented");
+  }
+
+}
