a4e82247ea1117526e1b39cfb86273739fd9c32c
mahuerta <mahuerta@qualityobjects.com>
2020-11-15 21:46:48 +0100

    Make PieceBuilderTest.

--- src/test/java/usantatecla/draughts/models/PieceBuilderTest.java
+++ src/test/java/usantatecla/draughts/models/PieceBuilderTest.java
@@ -1,6 +1,46 @@
 package usantatecla.draughts.models;
 
-// TODO: Code this test
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
 public class PieceBuilderTest {
 
-}
+  PieceBuilder pieceBuilder;
+
+  @Before
+  public void prepareGameBuilder() {
+    this.pieceBuilder = new PieceBuilder();
+  }
+
+  @Test(expected = AssertionError.class)
+  public void testGivenPieceBuilderWhenNullColorThenError() {
+  this.pieceBuilder.color(null).build();
+  }
+
+  @Test
+  public void testGivenPieceBuilderWhenNullTypeThenNullPiece() {
+    assertEquals(null,this.pieceBuilder.piece(null).build());
+  }
+
+  @Test
+  public void testGivenPieceBuilderWhenPawnTypeThenOK() {
+    assertEquals(new Pawn(Color.WHITE),this.pieceBuilder.piece("p").build());
+  }
+
+  @Test
+  public void testGivenPieceBuilderWhenDraughtTypeThenOK() {
+    assertEquals(new Draught(Color.WHITE),this.pieceBuilder.piece("d").build());
+  }
+
+  @Test
+  public void testGivenPieceBuilderWhenWhiteColorThenOK() {
+    assertEquals(Color.WHITE,this.pieceBuilder.color(Color.WHITE).build().getColor());
+  }
+
+  @Test
+  public void testGivenPieceBuilderWhenBlackColorThenOK() {
+    assertEquals(Color.BLACK,this.pieceBuilder.color(Color.BLACK).build().getColor());
+  }
+  }
