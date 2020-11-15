e423b9239af0e032397ccd51882fd419f25983c2
cd <jc-blazquez@hotmail.com>
2020-11-15 21:56:35 +0100

    Refactor

--- src/test/java/usantatecla/draughts/views/PlayViewTest.java
+++ src/test/java/usantatecla/draughts/views/PlayViewTest.java
@@ -79,7 +79,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractThenCancel() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("-1").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("-1").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).cancel();
   }
@@ -87,7 +87,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithEmptyThenError() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -95,7 +95,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithNotPointThenError() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("87,68").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("87,68").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -104,7 +104,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithBadFormatThenError() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("a3.42").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("a3.42").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -112,7 +112,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithBadRangeThenError() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("93.49").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("93.49").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -120,7 +120,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithNegativeThenError() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("43.-34").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("43.-34").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -128,7 +128,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithSecondNegativeThenError() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("4-3.34").thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("4-3.34").thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -136,7 +136,7 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithTwoCoordiantesThenOk() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("32.41");
     playView.interact(playController);
     verify(playController).move(new Coordinate(2, 1), new Coordinate(3, 0));
   }
@@ -144,11 +144,8 @@ public class PlayViewTest {
   @Test
   public void testGivenPlayViewWhenInteractWithThirdCoordiantesThenOk() {
     when(playController.getColor()).thenReturn(Color.BLACK);
-    when(console.readString("Mueven las negras: ")).thenReturn("23.32.41");
+    when(console.readString(Mockito.anyString())).thenReturn("23.32.41");
     playView.interact(playController);
-    verify(playController).move(
-        new Coordinate(1, 2),
-        new Coordinate(2, 1),
-        new Coordinate(3, 0));
+    verify(playController).move(new Coordinate(1, 2), new Coordinate(2, 1), new Coordinate(3, 0));
   }
 }
