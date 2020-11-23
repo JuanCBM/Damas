ca694c84ae99be182465a2525f412b488e80b25e
juablaz <jc-blazquez@hotmail.com>
2020-11-23 19:21:46 +0100

    Formateo

--- src/main/java/usantatecla/draughts/utils/Console.java
+++ src/main/java/usantatecla/draughts/utils/Console.java
@@ -5,68 +5,66 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Console {
-	
-	private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(
-			System.in));
 
-	public String readString(String title) {
-		String input = null;
-		boolean ok = false;
-		do {
-			this.write(title);
-			try {
-				input = bufferedReader.readLine();
-				ok = true;
-			} catch (IOException ex) {
-				this.writeError("de cadena de caracteres");
-			}
-		} while (!ok);
-		return input;
-	}
+  private BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-	public int readInt(String title) {
-		int input = 0;
-		boolean ok = false;
-		do {
-			try {
-				input = Integer.parseInt(this.readString(title));
-				ok = true;
-			} catch (NumberFormatException ex) {
-				this.writeError("entero");
-			}
-		} while (!ok);
-		return input;
-	}
+  public String readString(String title) {
+    String input = null;
+    boolean ok = false;
+    do {
+      this.write(title);
+      try {
+        input = bufferedReader.readLine();
+        ok = true;
+      } catch (IOException ex) {
+        this.writeError("de cadena de caracteres");
+      }
+    } while (!ok);
+    return input;
+  }
 
-	public char readChar(String title) {
-		char charValue = ' ';
-		boolean ok = false;
-		do {
-			String input = this.readString(title);
-			if (input.length() != 1) {
-				this.writeError("caracter");
-			} else {
-				charValue = input.charAt(0);
-				ok = true;
-			}
-		} while (!ok);
-		return charValue;
-	}
+  public int readInt(String title) {
+    int input = 0;
+    boolean ok = false;
+    do {
+      try {
+        input = Integer.parseInt(this.readString(title));
+        ok = true;
+      } catch (NumberFormatException ex) {
+        this.writeError("entero");
+      }
+    } while (!ok);
+    return input;
+  }
 
-	public void writeln() {
-		System.out.println();
-	}
-	
-	public void write(String string) {
-		System.out.print(string);
-	}
+  public char readChar(String title) {
+    char charValue = ' ';
+    boolean ok = false;
+    do {
+      String input = this.readString(title);
+      if (input.length() != 1) {
+        this.writeError("caracter");
+      } else {
+        charValue = input.charAt(0);
+        ok = true;
+      }
+    } while (!ok);
+    return charValue;
+  }
 
-	public void writeln(String string) {
-		System.out.println(string);
-	}
+  public void writeln() {
+    System.out.println();
+  }
 
-	private void writeError(String formato) {
-		System.out.println("ERROR DE FORMATO! "
-				+ "Introduzca un valor con formato " + formato + ".");
-	}
+  public void write(String string) {
+    System.out.print(string);
+  }
+
+  public void writeln(String string) {
+    System.out.println(string);
+  }
+
+  private void writeError(String formato) {
+    System.out.println("ERROR DE FORMATO! " + "Introduzca un valor con formato " + formato + ".");
+  }
 }
