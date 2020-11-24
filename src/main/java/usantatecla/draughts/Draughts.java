0923aa72970bf71a3c215f36981165810539feed
juablaz <jc-blazquez@hotmail.com>
2020-11-24 20:09:05 +0100

    Solving wrong commit

--- src/main/java/usantatecla/draughts/Draughts.java
+++ src/main/java/usantatecla/draughts/Draughts.java
@@ -1,39 +1,30 @@
 package usantatecla.draughts;
 
-import usantatecla.draughts.controllers.InteractorController;
 import usantatecla.draughts.controllers.Logic;
+import usantatecla.draughts.controllers.InteractorController;
 import usantatecla.draughts.views.View;
 
 class Draughts {
+    
+    private View view;
+    private Logic logic;
 
-  private View view;
-  private Logic logic;
+    private Draughts(){
+        this.view = new View();
+        this.logic = new Logic();
+    }
 
-  private Draughts() {
+    private void play() {
+        InteractorController controller;
+		do {
+			controller = this.logic.getController();
+			if (controller != null)
+				this.view.interact(controller);
+		} while (controller != null); 
+    }
 
-
-    int[] numbers = new int[10];
-
-    this.view = new View();
-    this.logic = new Logic();
-    System.out.println(new Float(3.14));
-  }
-
-  private Draughts(String ha) {
-
-  }
-
-  private void play() {
-    InteractorController controller;
-    do {
-      controller = this.logic.getController();
-      if (controller != null)
-        this.view.interact(controller);
-    } while (controller != null);
-  }
-
-  public static void main(String[] args) {
-    new Draughts().play();
-  }
-
+    public static void main(String[] args){
+        new Draughts().play();
+    }
+    
 }
