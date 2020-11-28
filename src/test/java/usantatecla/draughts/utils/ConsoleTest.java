d73fbdeb7a6eaf7b932262ea46b0987d8752d500
juablaz <jc-blazquez@hotmail.com>
2020-11-28 23:11:33 +0100

    [Test-Refactor] Actualizando test a version del pom

--- src/test/java/usantatecla/draughts/utils/ConsoleTest.java
+++ src/test/java/usantatecla/draughts/utils/ConsoleTest.java
@@ -1,56 +1,50 @@
 package usantatecla.draughts.utils;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import java.io.BufferedReader;
 import java.io.IOException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
-
+@RunWith(MockitoJUnitRunner.class)
 public class ConsoleTest {
 
-    @InjectMocks
-    Console console;
+  @InjectMocks
+  Console console;
 
-    @Mock
-    BufferedReader bufferedReader;
+  @Mock
+  BufferedReader bufferedReader;
 
-    @Before
-    public void before() {
-        initMocks(this);
-    }
+  @Test
+  public void testReadStringWhenIOExceptionThenAskAgain() throws IOException {
+    String expected = "Prueba OK";
+    when(bufferedReader.readLine()).thenThrow(new IOException()).thenReturn(expected);
+    console.readString("testReadStringWhenIOExceptionThenAskAgain");
+    verify(bufferedReader, times(2)).readLine();
+    assertEquals(expected, console.readString("testReadStringWhenIOExceptionThenAskAgain"));
+  }
 
-    @Test
-    public void testReadStringWhenIOExceptionThenAskAgain() throws IOException {
-        String expected = "Prueba OK";
-        when(bufferedReader.readLine())
-                .thenThrow(new IOException())
-                .thenReturn(expected);
-        console.readString("testReadStringWhenIOExceptionThenAskAgain");
-        verify(bufferedReader, times(2)).readLine();
-        assertEquals(expected, console.readString("testReadStringWhenIOExceptionThenAskAgain"));
-    }
+  @Test
+  public void testReadNumberWhenNumberFormatExceptionThenAskAgain() throws IOException {
+    String expected = "9999";
+    when(bufferedReader.readLine()).thenReturn("xxxx", expected);
+    int resultado = console.readInt("testReadNumberWhenNumberFormatExceptionThenAskAgain");
+    verify(bufferedReader, times(2)).readLine();
+    assertEquals(Integer.parseInt(expected), resultado);
+  }
 
-    @Test
-    public void testReadNumberWhenNumberFormatExceptionThenAskAgain() throws IOException {
-        String expected = "9999";
-        when(bufferedReader.readLine()).thenReturn("xxxx", expected);
-        int resultado = console.readInt("testReadNumberWhenNumberFormatExceptionThenAskAgain");
-        verify(bufferedReader, times(2)).readLine();
-        assertEquals(Integer.parseInt(expected), resultado);
-    }
-
-    @Test
-    public void testReadCharWhenLongerThanOneThenAskAgain() throws IOException {
-        char expected = 'y';
-        when(bufferedReader.readLine()).thenReturn("xxxx", expected + "");
-        char resultado = console.readChar("testReadCharWhenLongerThanOneThenAskAgain");
-        verify(bufferedReader, times(2)).readLine();
-        assertEquals(expected, resultado);
-    }
+  @Test
+  public void testReadCharWhenLongerThanOneThenAskAgain() throws IOException {
+    char expected = 'y';
+    when(bufferedReader.readLine()).thenReturn("xxxx", expected + "");
+    char resultado = console.readChar("testReadCharWhenLongerThanOneThenAskAgain");
+    verify(bufferedReader, times(2)).readLine();
+    assertEquals(expected, resultado);
+  }
 }
--- src/test/java/usantatecla/draughts/utils/YesNoDialogTest.java
+++ src/test/java/usantatecla/draughts/utils/YesNoDialogTest.java
@@ -1,54 +1,51 @@
 package usantatecla.draughts.utils;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.MockitoAnnotations.initMocks;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 
+@RunWith(MockitoJUnitRunner.class)
 public class YesNoDialogTest {
 
-    @InjectMocks
-    private YesNoDialog yesNoDialog;
+  @InjectMocks
+  private YesNoDialog yesNoDialog;
 
-    @Mock
-    Console console;
+  @Mock
+  Console console;
 
-    private final char AFFIRMATIVE = 'y';
-    private final char NEGATIVE = 'n';
-    private final String ERROR = "The value must be '" + AFFIRMATIVE + "' or '" + NEGATIVE + "'";
+  private final char AFFIRMATIVE = 'y';
+  private final char NEGATIVE = 'n';
+  private final String ERROR = "The value must be '" + AFFIRMATIVE + "' or '" + NEGATIVE + "'";
 
-    @Before
-    public void before() {
-        initMocks(this);
-    }
+  @Test(expected = AssertionError.class)
+  public void testReadWhenTitleIsNull() {
+    this.yesNoDialog.read(null);
+  }
 
-    @Test(expected = AssertionError.class)
-    public void testReadWhenTitleIsNull() {
-        this.yesNoDialog.read(null);
-    }
+  @Test
+  public void testAnswerYes() {
+    when(this.console.readChar(any())).thenReturn(this.AFFIRMATIVE);
+    assertTrue(this.yesNoDialog.read("title"));
+  }
 
-    @Test
-    public void testAnswerYes() {
-        when(this.console.readChar(any())).thenReturn(this.AFFIRMATIVE);
-        assertTrue(this.yesNoDialog.read("title"));
-    }
+  @Test
+  public void testAnswerNo() {
+    when(this.console.readChar(any())).thenReturn(this.NEGATIVE);
+    assertFalse(this.yesNoDialog.read("title"));
+  }
 
-    @Test
-    public void testAnswerNo() {
-        when(this.console.readChar(any())).thenReturn(this.NEGATIVE);
-        assertFalse(this.yesNoDialog.read("title"));
-    }
-
-    @Test
-    public void testWrongAnswer() {
-        when(this.console.readChar(any())).thenReturn('x', this.NEGATIVE);
-        this.yesNoDialog.read("title");
-        verify(console, times(1)).writeln(ERROR);
-    }
+  @Test
+  public void testWrongAnswer() {
+    when(this.console.readChar(any())).thenReturn('x', this.NEGATIVE);
+    this.yesNoDialog.read("title");
+    verify(console, times(1)).writeln(ERROR);
+  }
 }
